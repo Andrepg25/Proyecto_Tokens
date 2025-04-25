e49a60b08b1f0bf82fd37cfbbbc30b962d05ee42
Andrepg25 <andreapareja80@gmail.com>
2025-04-24 20:32:46 -0500

    Initial commit

---
+++ Constants/UserConstants.cs
@@ -0,0 +1,28 @@
+﻿using Proyecto_Tokens.Models;
+using System.Collections.Generic;
+
+namespace Proyecto_Tokens.Constants
+{
+    public static class UserConstants
+    {
+        public static List<UserModels> Users = new List<UserModels>
+        {
+            new UserModels
+            {
+                ID = 1,
+                Nombre_Usuario = "admin",
+                Contraseña = "admin123",
+                Correo_Electronico = "admin@gmail.com",
+                Rol = "Administrador"
+            },
+            new UserModels
+            {
+                ID = 2,
+                Nombre_Usuario = "cliente",
+                Contraseña = "cliente123",
+                Correo_Electronico = "cliente@gmail.com",
+                Rol = "Cliente"
+            }
+        };
+    }
+}
